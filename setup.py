dd35cb60ce173e1516d509be7fcdad2742b13bd4
Albert Zeyer <zeyer@cs.rwth-aachen.de>
2019-02-02 16:25:02 +0100

    setup.py: use version via Git commit

--- setup.py
+++ setup.py
@@ -35,6 +35,8 @@ from distutils.core import setup
 import time
 from pprint import pprint
 import os
+import sys
+from subprocess import Popen, check_output, PIPE
 
 
 def debug_print_file(fn):
@@ -63,14 +65,50 @@ def parse_pkg_info(fn):
     return res
 
 
+def git_commit_rev(commit="HEAD", git_dir="."):
+    if commit is None:
+        commit = "HEAD"
+    return check_output(["git", "rev-parse", "--short", commit], cwd=git_dir).decode("utf8").strip()
+
+
+def git_is_dirty(git_dir="."):
+    proc = Popen(["git", "diff", "--no-ext-diff", "--quiet", "--exit-code"], cwd=git_dir, stdout=PIPE)
+    proc.communicate()
+    if proc.returncode == 0:
+        return False
+    if proc.returncode == 1:
+        return True
+    raise Exception("unexpected return code %i" % proc.returncode)
+
+
+def git_commit_date(commit="HEAD", git_dir="."):
+    out = check_output(["git", "show", "-s", "--format=%ci", commit], cwd=git_dir).decode("utf8")
+    out = out.strip()[:-6].replace(":", "").replace("-", "").replace(" ", ".")
+    return out
+
+
+def git_head_version(git_dir="."):
+    commit_date = git_commit_date(git_dir=git_dir)  # like "20190202.154527"
+    # rev = git_commit_rev(git_dir=git_dir)
+    # is_dirty = git_is_dirty(git_dir=git_dir)
+    # Make this distutils.version.StrictVersion compatible.
+    return "1.%s" % commit_date
+
+
 if os.path.exists("PKG-INFO"):
     print("Found existing PKG-INFO.")
     info = parse_pkg_info("PKG-INFO")
     version = info["Version"]
     print("Version via PKG-INFO:", version)
 else:
-    version = time.strftime("1.%Y%m%d.%H%M%S", time.gmtime())
-    print("Version via current time:", version)
+    try:
+        version = git_head_version()
+        print("Version via Git:", version)
+    except Exception as exc:
+        print("Exception while getting Git version:", exc)
+        sys.excepthook(*sys.exc_info())
+        version = time.strftime("1.%Y%m%d.%H%M%S", time.gmtime())
+        print("Version via current time:", version)
 
 
 if os.environ.get("DEBUG", "") == "1":
